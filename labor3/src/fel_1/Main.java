c72a833b51ebf9e87bf1a7e59936426b8bc5f935
Dumbrăvean-Kátai David <dkdavid24@live.com>
2021-03-01 10:52:49 +0200

    Labor 3 fel 1

--- labor3/src/fel_1/Main.java
+++ labor3/src/fel_1/Main.java
@@ -1,4 +1,23 @@
 package fel_1;
 
 public class Main {
+    public static void main(String[] args){
+        Customer c1 = new Customer("John", "Black");
+        System.out.println(c1);
+        c1.setAccount(new BankAccount("OTP00001"));
+        System.out.println(c1);
+        c1.getAccount().deposit(1000);
+        System.out.println(c1);
+        Customer c2 = new Customer("Mary", "White");
+        c2.setAccount(new BankAccount(("OTP00002")));
+        System.out.println(c2);
+        c2.getAccount().deposit(1000);
+        System.out.println(c2);
+        c2.getAccount().withdraw(500);
+        System.out.println(c2);
+        c2.closeAccount();
+        System.out.println(c2);
+        c2.setAccount(c1.getAccount());
+        System.out.println(c2);
+    }
 }
