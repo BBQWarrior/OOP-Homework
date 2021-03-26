6c41527cce2d6d8ccbb6a4b4a030cff2a2bd16ce
Dumbrăvean-Kátai David <dkdavid24@live.com>
2021-03-26 22:07:40 +0200

    lab 6 fel 2

--- labor6/src/fel_2/Main.java
+++ labor6/src/fel_2/Main.java
@@ -1,31 +1,27 @@
 package fel_2;
 
-import java.util.Random;
-
 public class Main {
     public static void main(String[] args) {
-        String str = "name name";
-        StackAggregation stack1 = new StackAggregation(str.length());
-
-        for(char c : str.toCharArray()){
-            stack1.push(c);
+        StackAggregation stack1 = new StackAggregation(5);
+        for (int i = 0; i < 10; ++i) {
+            // boxing: int --> Integer
+            stack1.push(i);
         }
-        while (!stack1.isEmpty()){
-            System.out.print(stack1.top());
+        System.out.print("StackAggregation : ");
+        while (!stack1.isEmpty()) {
+            System.out.print(stack1.top() + " ");
             stack1.pop();
         }
         System.out.println();
-        
-        StackAggregation stack2 = new StackAggregation(10);
-        Random rand = new Random();
-        for(int i=0;i<13;i++){
-            int temp = rand.nextInt(1000);
-            System.out.print(temp + ", ");
-            stack2.push(temp);
-        }
-        System.out.println();
 
-        while (!stack2.isEmpty()){
+        StackInheritance stack2 = new StackInheritance(5);
+
+        for (int i = 0; i < 10; ++i) {
+            stack2.push(i);
+        }
+        stack2.remove(1);
+        System.out.print("StackInheritance : ");
+        while (!stack2.isEmpty()) {
             System.out.print(stack2.top() + " ");
             stack2.pop();
         }
--- labor6/src/fel_2/StackInheritance.java
+++ labor6/src/fel_2/StackInheritance.java
@@ -4,9 +4,44 @@ import java.util.ArrayList;
 
 public class StackInheritance extends ArrayList {
     private int capacity;
-
+    
     public StackInheritance(int capacity) {
         this.capacity = capacity;
     }
 
+    public void push(Object obj) {
+        if (this.isFull()) {
+            System.out.println("Stack is full");
+            return;
+        }
+        this.add(obj);
+    }
+
+    public void pop() {
+        if (this.isEmpty()) {
+            System.out.println("Stack is empty");
+            return;
+        }
+        this.remove(this.size() - 1);
+    }
+
+    public Object top() {
+        if (this.isEmpty()) {
+            System.out.println("Stack is empty");
+            return null;
+        }
+        return this.get(this.size() - 1);
+    }
+
+    public int getSize() {
+        return this.size();
+    }
+
+    public boolean isEmpty() {
+        return this.size() == 0;
+    }
+
+    public boolean isFull() {
+        return this.size() == capacity;
+    }
 }
